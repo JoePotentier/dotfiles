cbe72e829089405fa1088cf8b4d7b685f7ca5963
Joseph Potentier <9673684+JoePotentier@users.noreply.github.com>
2023-07-01 17:40:44 -0600

    Added zshenv with exists function, hooked up with dotbot, used new function to speed up homebrew installs

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -2,7 +2,13 @@
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if exists brew; then
+  echo "brew exists, skipping install"
+else
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 
 # Note: Currently you can't do `brew bundle --no-quarantine` as an option.
 # It's currently exported in zshrc:
