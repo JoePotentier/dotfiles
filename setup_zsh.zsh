0a425a14426c3874fc4afea7a582c8bf9b599f55
Joseph Potentier <9673684+JoePotentier@users.noreply.github.com>
2023-07-02 13:18:42 -0600

    Added conditionals to setup

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,9 +4,27 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 
 # Installation unnecessary; it's in the Brewfile.
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+HOMEBREW_ZSH_PATH="$HOMEBREW_REPOSITORY/bin/zsh"
+SHELLS_PATH='/etc/shells'
+if grep -q "$HOMEBREW_ZSH_PATH" "$SHELLS_PATH"; then
+  echo "Homebrew zsh, already exists in $SHELLS_PATH, skipping\n"
+else
+  echo "Enter superuser (sudo) password to edit $SHELLS_PATH"
+  echo "$HOMEBREW_ZSH_PATH" | sudo tee -a "$SHELLS_PATH">/dev/null
+fi  
 
-echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh'
+if [[ "$SHELL" == "$HOMEBREW_ZSH_PATH" ]]; then
+  echo "Login shell already set to $HOMEBREW_ZSH_PATH, skipping\n"
+else
+  echo "Enter user password to change login shell"
+  chsh -s "$HOMEBREW_ZSH_PATH"
+fi
+
+if exa /private/var/select/sh | grep -q zsh ; then
+  echo "Command sh already symlinked to zsh, skipping\n"
+else 
+  echo "Enter superuser (sudo) password to symlink sh to zsh"
+  # Looked cute, might delete later, idk (homebrew zsh was not working)
+  sudo ln -sfv /bin/zsh /private/var/select/sh 
+fi
 
